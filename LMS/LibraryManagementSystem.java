cb95464eab71dee86f57f379c7d453b9bab17d17
BalajiSelvam <balajiselvam245@gmail.com>
2025-09-29 09:11:00 +0530

    2 applications added

---
+++ LMS/LibraryManagementSystem.java
@@ -0,0 +1,304 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+//BOOKS
+class Book{
+    String bookId;
+    String bookName;
+    String author;
+    int quantity;
+
+    Book(String bookId, String bookName, String author, int quantity){
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.author = author;
+        this.quantity = quantity;
+    }
+
+    public String toString(){
+        return "{ " + bookId + ", " + bookName + ", " + author + ", " + quantity + ", " + " }";
+    }
+}
+
+//MEMBERS
+class Member{
+    String memberId;
+    String memberName;
+
+    Member(String memberId, String memberName){
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public String toString(){
+        return "{ " + memberId + ", " + memberName + "} ";
+    }
+}
+
+//LIBRARY MANAGEMENT
+class Library{
+    List<Book> books = new ArrayList<>();
+    List<Member> members = new ArrayList<>();
+    Map<String, List<String>> issued = new HashMap<>();
+
+    int totalBooks = 0;
+    int totalMembers = 0;
+
+    //Adding books
+    public void addBooks(Book b){
+        books.add(b);
+        totalBooks++;
+        System.out.println("Book " + b.bookId + " added succesfully");
+    }
+
+    //Display books
+    public void displayBooks(){
+        if(books.isEmpty()){
+            System.out.println("No Books to display");
+            return;
+        }
+        for(Book b : books){
+            System.out.println(b);
+        }
+        System.out.println("Total Books : " + totalBooks);
+        
+    }
+
+    //Remove Books
+    public void removeBooks(String bOOkID){
+        for(Book b : books){
+            if(b.bookId.equals(bOOkID)){
+                books.remove(b);
+                System.out.println("Book removed Sucessfully");
+                return;
+            }
+        }
+        System.out.println("No Book exists with the given BookId");
+    }
+
+    //Search books
+    public void searchBooks(String title){
+        for(Book b : books){
+            if(b.bookName.equalsIgnoreCase(title)){
+                System.out.println("Book " + title + " found");
+                return;
+            }
+        }
+        System.out.println("Book " + title + " not found");
+    }
+
+    //Adding Members
+    public void addMembers(Member m){
+        members.add(m);
+        System.out.println("Member " + m.memberId + " added succesfully");
+    }
+
+    //Display Members
+    public void displayMembers(){
+        if(members.isEmpty()){
+            System.out.println("No members to display");
+            return;
+        }
+        for(Member m : members){
+            totalMembers++;
+            System.out.println(m);
+        }
+        System.out.println("Total members : " + totalMembers);
+    }
+
+    //Remove Members
+    public void removeMembers(String memId){
+        for(Member m : members){
+            if(m.memberId.equals(memId)){
+                members.remove(m);
+                System.out.println("Member removed successfully");
+                return;
+            }
+        }
+        System.out.println("MemberId not exist");
+
+    }
+    //Issue books
+    public void issueBooks(String bId, String mId){
+        for(Book b : books){
+            if(b.bookId.equalsIgnoreCase(bId)){
+                if(b.quantity > 0){
+                    b.quantity--;
+                    issued.putIfAbsent(bId, new ArrayList<>());
+                    issued.get(bId).add(mId);
+                    System.out.println("Book " + bId + " issued to " + mId + " Succesfully" );
+                    return;
+                }
+                else{
+                    System.out.println("Book not available");
+                    return;
+                }
+            }
+        }
+        System.out.println("Invalid BookId");    
+    }
+
+    //Return Books
+    public void returnBooks(String bookId, String memberId){
+        if(issued.containsKey(bookId) && issued.get(bookId).contains(memberId)){
+            for(Book b : books){
+                if(b.bookId.equals(bookId)){
+                    b.quantity++;
+                    issued.get(bookId).remove(memberId);
+                    if(issued.get(bookId).isEmpty()){
+                        issued.remove(bookId);
+                    }
+                System.out.println("Book " + bookId + " returned by " + memberId);
+                return;
+                }
+            }
+           
+        }
+        else{
+                System.out.println("Invalid Operation");
+        }
+    }
+
+    //Track Books
+    public void trackBooks(String bookId){
+    for(Book b : books){
+        if(b.bookId.equals(bookId)){
+            int available = b.quantity;
+            int totalIssued = issued.containsKey(bookId) ? issued.get(bookId).size() : 0;
+
+            System.out.println("\nTracking Book: " + bookId + " (" + b.bookName + ")");
+            System.out.println("Available copies: " + available);
+            System.out.println("Issued copies: " + totalIssued);
+
+            if(totalIssued > 0){
+                System.out.println("Issued to members: " + issued.get(bookId));
+            } else {
+                System.out.println("No members currently holding this book.");
+            }
+            return;
+        }
+    }
+    System.out.println("Invalid Book Id");
+}
+
+    //Book availability
+    public void checkAvailability(String bookIdd){
+        for(Book b : books){
+            if(b.bookId.equals(bookIdd)){
+                if(b.quantity!=0){
+                    System.out.println("Book available. Ready fo Issue");
+                    return;
+                }
+                else{
+                    System.out.println("Sorry Book not availble. Come after some time");
+                    return;
+                }
+            }
+        }
+        System.out.println("Invalid Book Id");
+    }
+}
+
+public class LibraryManagementSystem{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Library lb = new Library();
+
+        while(true){
+            System.out.println("\n=====LIBRARY MANAGEMENT SYSTEM=====");
+            System.out.println("1. Add Book");
+            System.out.println("2. Display Books");
+            System.out.println("3. Remove Books");
+            System.out.println("4. Search Books");
+            System.out.println("5. Add Member");
+            System.out.println("6. Display Members");
+            System.out.println("7. Remove Members");
+            System.out.println("8. Issue Book");
+            System.out.println("9. Return Book");
+            System.out.println("10. Check Book Availability");
+            System.out.println("11. Track books");
+            System.out.println("12. Exit");
+
+            System.out.print("\nEnter your choice : ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice){
+                case 1:
+                    System.out.print("Enter Book Id : ");
+                    String id = sc.nextLine();
+                    System.out.print("Enter Book Title : ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Author name : ");
+                    String aname = sc.nextLine();
+                    System.out.print("Enter Quantity : ");
+                    int qty = sc.nextInt();
+                    lb.addBooks(new Book(id, name, aname, qty));
+                    break;
+                case 2:
+                    lb.displayBooks();
+                    break;
+                case 3:
+                    System.out.print("Enter the BookId to remove : ");
+                    String bIId = sc.nextLine();
+                    lb.removeBooks(bIId);
+                    break;
+                case 4:
+                    System.out.print("Enter the Book title to search : ");
+                    String btitle = sc.nextLine();
+                    lb.searchBooks(btitle);
+                    break;
+                case 5:
+                    System.out.print("Enter Member Id : ");
+                    String mId = sc.nextLine();
+                    System.out.print("Enter member name : ");
+                    String mname = sc.nextLine();
+                    lb.addMembers(new Member(mId, mname));
+                    break;
+                case 6:
+                    lb.displayMembers();
+                    break;
+                case 7:
+                    System.out.println("Enter the MemberId to remove : ");
+                    String membId = sc.nextLine();
+                    lb.removeMembers(membId);
+                    break;
+                case 8:
+                    System.out.print("Enter the Book Id to get : ");
+                    String bId = sc.nextLine();
+                    System.out.print("Please provide your member Id : ");
+                    String mIdd = sc.nextLine();
+                    lb.issueBooks(bId, mIdd);
+                    break;
+                case 9:
+                    System.out.print("Enter the Book Id to return : ");
+                    String bIdd = sc.nextLine();
+                    System.out.print("Enter your MemberId : ");
+                    String mIddd = sc.nextLine();
+                    lb.returnBooks(bIdd, mIddd);
+                    break;
+                case 10:
+                    System.out.print("Enter the Book Id to check availability : ");
+                    String bookID = sc.nextLine();
+                    lb.checkAvailability(bookID);
+                    break;
+                case 11:
+                    System.out.print("Enter the Book Id to track : ");
+                    String tbId = sc.nextLine();
+                    lb.trackBooks(tbId);
+                    break;
+                case 12:
+                    System.out.println("Exiting... Bye Bye");
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("Invalid Choice try again");
+            }
+        }
+    }
+    
+}
---
+++ SMS/StudentManagementSystem.java
@@ -0,0 +1,292 @@
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+class Student{
+    String rollNo;
+    String name;
+    String department;
+    String yearOfStudy;
+    Map<String, Integer> marks = new HashMap<>();
+    Student(String rollNo, String name, String department, String yearOfStudy){
+        this.rollNo = rollNo;
+        this.name = name;
+        this.department = department;
+        this.yearOfStudy = yearOfStudy;
+    }
+
+    public String toString(){
+        return "[ " + rollNo + ", " + name + ", " + department + ", " + yearOfStudy 
+            + ", Marks=" + marks + " ]";
+    }
+
+}
+
+//STUDENT MANAGEMENT
+class StudentRecords{
+    Scanner sc = new Scanner(System.in);
+    List<Student> students = new ArrayList<>();
+    int totalStudents = 0;
+
+
+    //Add Students
+    public void addStudents(Student s){
+        totalStudents++;
+        students.add(s);
+        System.out.println("Student " + s.rollNo + " was added Sucessfully");
+    }
+
+    //Display Students
+    public void displayStudents(){
+        if(students.isEmpty()){
+            System.out.println("No Students to dispplay");
+            return;
+        }
+        for(Student s : students){
+            System.out.println(s);
+        }
+        System.out.println("Total students : "+totalStudents);
+    }
+
+    //Update Students
+    public void updateStudents(String rollNo){
+        int flag = 0;
+        for(Student s : students){
+            if(s.rollNo.equalsIgnoreCase(rollNo)){
+                flag = 1;
+                System.out.print("Edit name (Yes/No) : ");
+                String resp = sc.nextLine();
+                if(resp.equalsIgnoreCase("yes")){
+                    System.out.print("Enter the updated name : ");
+                    String newName = sc.nextLine();
+                    s.name = newName;
+                }
+                System.out.print("Edit department (Yes/No) : ");
+                String resp1 = sc.nextLine();
+                if(resp1.equalsIgnoreCase("yes")){
+                    System.out.print("Enter the updated department : ");
+                    String newDept = sc.nextLine();
+                    s.department = newDept;
+                }
+                System.out.println("Edit year (Yes/No) : ");
+                String resp2 = sc.nextLine();
+                if(resp2.equalsIgnoreCase("yes")){
+                    System.out.print("Enter the updated year : ");
+                    String newYear = sc.nextLine();
+                    s.yearOfStudy = newYear;
+                }
+            }
+        }
+        if(flag==0){
+            System.out.println("Invalid Roll Number");
+        }
+    }
+
+    //Remove Students
+    public void removeStudents(String rollNO){
+        for(Student s : students){
+            if(s.rollNo.equalsIgnoreCase(rollNO)){
+                totalStudents--;
+                System.out.println("Student " + s.rollNo + " is removed sucessfully");
+                students.remove(s);
+                return;
+            }
+        }
+        System.out.println("Invalid Roll Number");
+    }
+
+    //Search Student by rollNo
+    public void searchStudent(String rollNOO){
+        for(Student s : students){
+            if(s.rollNo.equalsIgnoreCase(rollNOO)){
+                System.out.println(s);
+                return;
+            }
+        }
+    System.out.println("Invalid Roll Number");
+    }
+
+    //Add marks
+    public void addMarks(String rollNo) {
+        for(Student s : students){
+            if(s.rollNo.equalsIgnoreCase(rollNo)){
+                System.out.print("How many subjects ? ");
+                int noOfSubjects = sc.nextInt();
+                sc.nextLine();
+                for(int i=1; i<=noOfSubjects; i++){
+                    System.out.print("Enter subject name: ");
+                    String subject = sc.nextLine();
+                    System.out.print("Enter marks: ");
+                    int mark = sc.nextInt();
+                    sc.nextLine(); 
+                    s.marks.put(subject, mark);
+                }
+                System.out.println("Marks added for " + s.name);
+                return;
+            }
+        }
+        System.out.println("Invalid Roll Number");
+    }
+
+    //Calculate percentage
+    public void calculatePecentage(String rolllNo){
+        for(Student s : students){
+            if(s.rollNo.equalsIgnoreCase(rolllNo)){
+                if(s.marks.isEmpty()){
+                    System.out.println("No marks available for " + s.rollNo);
+                    return;
+                }
+                int total = 0;
+                for(int m : s.marks.values()){
+                    total += m;
+                }
+                double per = (double)(total / s.marks.size());
+                System.out.println("Percentage : " + per);
+                if(per>=90){
+                    System.out.println("Remarks : Execellent");
+                }
+                else if(per>=80 && per<=90){
+                    System.out.println("Remarks : Good");
+                }
+                else if (per>=60 && per<=80){
+                    System.out.println("Remarks : Fair");
+                }
+                else{
+                    System.out.println("Remarks : Need to Improve");
+                }
+                return;
+            }
+        }
+        System.out.print("Invalid roll number");
+    }
+
+    //Generate Report
+    public void generateReport(String rnum){
+        for(Student s : students){
+            if(s.rollNo.equalsIgnoreCase(rnum)){
+                System.out.println("Student Name : " + s.name);
+                System.out.println("Student Roll Number : " + s.rollNo);
+                System.out.println("Student Department : " + s.department);
+                System.out.println("Student Study Year : " + s.yearOfStudy);
+                System.out.println("Number of Subjects Studied : " + s.marks.size());
+                int i = 1;
+                for(Map.Entry<String, Integer> entry : s.marks.entrySet()){
+                    String subname = entry.getKey();
+                    int submarks = entry.getValue();
+                    System.out.println("Subject-" + i++ + " : " + subname + " ... " + "Marks : " + submarks);
+                }
+                calculatePecentage(rnum);
+                return;
+            }
+        }
+        System.out.println("Invalid Roll Number");
+    }
+
+    //Download Report
+    public void saveStudentsList(String fileName) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            if (students.isEmpty()) {
+                bw.write("No students available.");
+            } else {
+                for (Student s : students) {
+                    bw.write(s.toString() + "\n");
+                }
+            }
+            System.out.println("All students saved to file: " + fileName);
+        } catch (Exception e) {
+            System.out.println("Error while saving students: " + e.getMessage());
+        }
+    }
+
+
+
+}
+
+//STUDENT MANAGEMENT
+public class StudentManagementSystem{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentRecords sr = new StudentRecords();
+
+        while(true){
+            System.out.println("\n ===== STUDENT MANAGEMENT SYSTEM ===== ");
+            System.out.println("1. Add Students");
+            System.out.println("2. Update Student Details");
+            System.out.println("3. Remove Students");
+            System.out.println("4. Display Students");
+            System.out.println("5. Search Students");
+            System.out.println("6. Add marks");
+            System.out.println("7. Calculate Percentage");
+            System.out.println("8. Generate Report");
+            System.out.println("9. Download Students List");
+            System.out.println("10. Exit");
+
+            System.out.print("\nEnter your choice : ");
+            int choice = scan.nextInt();
+            scan.nextLine();
+
+            switch(choice){
+                case 1:
+                    System.out.print("Enter Student RollNumber : ");
+                    String rollNo = scan.nextLine();
+                    System.out.print("Enter Student Name : ");
+                    String name = scan.nextLine();
+                    System.out.print("Enter the department : ");
+                    String dept = scan.nextLine();
+                    System.out.print("Enter the Year of Stduy : ");
+                    String yearOfStudy = scan.nextLine();
+                    sr.addStudents(new Student(rollNo, name, dept, yearOfStudy));
+                    break;
+                case 2:
+                    System.out.print("Enter the Student Roll Number to edit : ");
+                    String rNo = scan.nextLine();
+                    sr.updateStudents(rNo);
+                    break;
+                case 3:
+                    System.out.print("Enter the Student Roll Number to remove : ");
+                    String rNoo = scan.nextLine();
+                    sr.removeStudents(rNoo);
+                    break;
+                case 4:
+                    sr.displayStudents();
+                    break;
+                case 5:
+                    System.out.print("Enter the Roll Number to Search : ");
+                    String rn = scan.nextLine();
+                    sr.searchStudent(rn);
+                    break;
+                case 6:
+                    System.out.print("Enter Roll Number: ");
+                    String rnMarks = scan.nextLine();
+                    sr.addMarks(rnMarks);
+                    break;
+                case 7:
+                    System.out.print("Enter the Student Roll Number : ");
+                    String rnr = scan.nextLine();
+                    sr.calculatePecentage(rnr);
+                    break;
+                case 8:
+                    System.out.print("Enter the Student Roll NUmber : ");
+                    String rnumber = scan.nextLine();
+                    sr.generateReport(rnumber);
+                    break;
+                case 9:
+                    System.out.println("Enter the file name in .txt format : ");
+                    String fname = scan.nextLine();
+                    sr.saveStudentsList(fname);
+                    break;
+                case 10:
+                    System.out.println("Exiting....\n");
+                    scan.close();
+                    return;
+                default:
+                    System.out.print("Invalid choice...");
+            }
+        }
+    }
+}
