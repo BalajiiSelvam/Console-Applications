d301b14b63bbee738bc6d927ad0da86e26e8bcfd
BalajiSelvam <balajiselvam245@gmail.com>
2025-09-29 15:23:30 +0530

    Complete LMS with newfeatures

--- LMS/LibraryManagementSystem.java
+++ LMS/LibraryManagementSystem.java
@@ -5,36 +5,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-/* * A SIMPLE LIBRARY MANAGEMENT SYSTEM WITH BELOW FEATURS
-   * 1. Add books
-   * 2. Display books
-   * 3. Remove books
-   * 4. Search books
-   * 5. Add Members
-   * 6. Remove Members
-   * 7. Display Members
-   * 8. Issue Books
-   * 9. Return books
-   * 10. Check book availability
-   * 11. Track Issues
-   * 12. Exit
-*/
-//BOOKS
 class Book{
     String bookId;
     String bookName;
     String author;
     int quantity;
-
     Book(String bookId, String bookName, String author, int quantity){
         this.bookId = bookId;
         this.bookName = bookName;
         this.author = author;
         this.quantity = quantity;
     }
-
     public String toString(){
-        return "{ " + bookId + ", " + bookName + ", " + author + ", " + quantity + ", " + " }";
+        return "{ ID : " + bookId + ", Name : " + bookName + ", Author : " + author + ", Quantity : " + quantity + " } ";
     }
 }
 
@@ -42,31 +25,63 @@ class Book{
 class Member{
     String memberId;
     String memberName;
-
     Member(String memberId, String memberName){
         this.memberId = memberId;
         this.memberName = memberName;
     }
-
     public String toString(){
-        return "{ " + memberId + ", " + memberName + "} ";
+        return "{ Name : " + memberName + ", ID : " + memberId + " } ";
     }
 }
 
-//LIBRARY MANAGEMENT
+//LIBRARY
 class Library{
+    Scanner scan = new Scanner(System.in);
     List<Book> books = new ArrayList<>();
     List<Member> members = new ArrayList<>();
-    Map<String, List<String>> issued = new HashMap<>();
+    Map<String,List<String>> issued = new HashMap<>();
 
-    int totalBooks = 0;
-    int totalMembers = 0;
-
-    //Adding books
+    //Adding Books
     public void addBooks(Book b){
         books.add(b);
-        totalBooks++;
-        System.out.println("Book " + b.bookId + " added succesfully");
+        System.out.println("Book " + b.bookId + " added successfully");
+    }
+
+    //Updating quantity
+    public void updateQuantity(String bookId){
+        for(Book b : books){
+            if(b.bookId.equals(bookId)){
+                System.out.print("Enter the available copies : ");
+                int newQuantity = scan.nextInt();
+                b.quantity = newQuantity;
+                System.out.println("Book Quantity updated sucessfully");
+                return;
+            }
+        }
+        System.out.println("Invalid BookId");
+    }
+
+    //Search books
+    public void searchBooks(String bookName){
+        for(Book b : books){
+            if(b.bookName.equalsIgnoreCase(bookName)){
+                System.out.println(b);
+                return;
+            }
+        }
+        System.out.println("Book Not Found");
+    }
+
+    //Remove books
+    public void removeBooks(String bookID){
+        for(Book b : books){
+            if(b.bookId.equals(bookID)){
+                System.out.println("Book " + bookID + " removed sucessfully");
+                books.remove(b);
+                return;
+            }
+        }
+        System.out.println("Invalid BookId");
     }
 
     //Display books
@@ -78,241 +93,264 @@ class Library{
         for(Book b : books){
             System.out.println(b);
         }
-        System.out.println("Total Books : " + totalBooks);
-        
-    }
-
-    //Remove Books
-    public void removeBooks(String bOOkID){
-        for(Book b : books){
-            if(b.bookId.equals(bOOkID)){
-                books.remove(b);
-                System.out.println("Book removed Sucessfully");
-                return;
-            }
-        }
-        System.out.println("No Book exists with the given BookId");
-    }
-
-    //Search books
-    public void searchBooks(String title){
-        for(Book b : books){
-            if(b.bookName.equalsIgnoreCase(title)){
-                System.out.println("Book " + title + " found");
-                return;
-            }
-        }
-        System.out.println("Book " + title + " not found");
+        System.out.println("\nTotal number of books : " + books.size());
     }
 
     //Adding Members
     public void addMembers(Member m){
         members.add(m);
-        System.out.println("Member " + m.memberId + " added succesfully");
+        System.out.println("Member " + m.memberId + " added successfully");
     }
 
-    //Display Members
+  //Display Members
     public void displayMembers(){
         if(members.isEmpty()){
             System.out.println("No members to display...");
             return;
         }
+
+    //Search Members
+    public void searchMembers(String memberId1){
         for(Member m : members){
-            totalMembers++;
-            System.out.println(m);
+            if(m.memberId.equals(memberId1)){
+                System.out.println(m);
+                return;
+            }
         }
-        System.out.println("Total members : " + totalMembers);
+        System.out.println("Invalid Member ID");
     }
 
     //Remove Members
-    public void removeMembers(String memId){
+    public void removeMembers(String memberId2){
         for(Member m : members){
-            if(m.memberId.equals(memId)){
+            if(m.memberId.equals(memberId2)){
+                System.out.println("Member " + memberId2 + " removed successfully");
                 members.remove(m);
-                System.out.println("Member removed successfully");
                 return;
             }
         }
-        System.out.println("MemberId not exist");
-
+        System.out.println("Invalid MemberId");
     }
+    
+    //Display Members
+    public void displayMembers(){
+        if(members.isEmpty()){
+            System.out.println("No Members to display");
+            return;
+        }
+        for(Member m : members){
+            System.out.println(m);
+        }
+        System.out.println("Total number of members : " + members.size());
+    }
+
     //Issue books
-    public void issueBooks(String bId, String mId){
+    private Book checkBookValidity(String bID){
         for(Book b : books){
-            if(b.bookId.equalsIgnoreCase(bId)){
-                if(b.quantity > 0){
-                    b.quantity--;
-                    issued.putIfAbsent(bId, new ArrayList<>());
-                    issued.get(bId).add(mId);
-                    System.out.println("Book " + bId + " issued to " + mId + " Succesfully" );
-                    return;
-                }
-                else{
-                    System.out.println("Book not available");
-                    return;
-                }
+            if(b.bookId.equals(bID)){
+                return b;
             }
         }
-        System.out.println("Invalid BookId");    
+        return null;
     }
-
-    //Return Books
-    public void returnBooks(String bookId, String memberId){
-        if(issued.containsKey(bookId) && issued.get(bookId).contains(memberId)){
-            for(Book b : books){
-                if(b.bookId.equals(bookId)){
-                    b.quantity++;
-                    issued.get(bookId).remove(memberId);
-                    if(issued.get(bookId).isEmpty()){
-                        issued.remove(bookId);
-                    }
-                System.out.println("Book " + bookId + " returned by " + memberId);
-                return;
-                }
+    private Member checkMemberValidity(String mID){
+        for(Member m : members){
+            if(m.memberId.equals(mID)){
+                return m;
             }
-           
+        }
+        return null;
+    }
+    public void issueBooks(String bId, String mId){
+        Book book = checkBookValidity(bId);
+        if(book==null){
+            System.out.println("Invalid BookId");
+            return;
+        }
+        Member member = checkMemberValidity(mId);
+        if(member==null){
+            System.out.println("Invalid MemberId");
+            return;
+        }
+        if(book.quantity > 0){
+            book.quantity--;
+            issued.putIfAbsent(bId, new ArrayList<>());
+            issued.get(bId).add(mId);
+            System.out.println("Book " + bId + " issued to Member " + mId);
         }
         else{
-                System.out.println("Invalid Operation");
+            System.out.println("Book not available at the moment");
         }
+    
+    }
+
+    //Return books
+    public void returnBooks(String bid, String mid){
+        if(issued.containsKey(bid) && issued.get(bid).contains(mid)){
+            for(Book b : books){
+                if(b.bookId.equals(bid)){
+                    b.quantity++;
+                    issued.get(bid).remove(mid);
+                    if(issued.get(bid).isEmpty()){
+                        issued.remove(bid);
+                    }
+                    System.out.println("Book " + bid + " returned by Member " + mid);
+                    return;
+                }
+            }
+        }
+        else{
+            System.out.println("Invalid credentials");
+        }
+    }
+
+    //Check Availability
+    public void checkAvailability(String bookid){
+        for(Book b : books){
+            if(b.bookId.equals(bookid)){
+                if(b.quantity > 0){
+                    System.out.println("Book " + bookid + " available for issue");
+                    return;
+                }
+                System.out.println("Book " + bookid + " not available at this moment");
+                return;
+            }
+        }
+        System.out.println("Invalid BookId");
     }
 
     //Track Books
-    public void trackBooks(String bookId){
-    for(Book b : books){
-        if(b.bookId.equals(bookId)){
-            int available = b.quantity;
-            int totalIssued = issued.containsKey(bookId) ? issued.get(bookId).size() : 0;
-
-            System.out.println("\nTracking Book: " + bookId + " (" + b.bookName + ")");
-            System.out.println("Available copies: " + available);
-            System.out.println("Issued copies: " + totalIssued);
-
-            if(totalIssued > 0){
-                System.out.println("Issued to members: " + issued.get(bookId));
-            } else {
-                System.out.println("No members currently holding this book.");
-            }
-            return;
-        }
-    }
-    System.out.println("Invalid Book Id");
-}
-
-    //Book availability
-    public void checkAvailability(String bookIdd){
+    public void trackBooks(String biD){
         for(Book b : books){
-            if(b.bookId.equals(bookIdd)){
-                if(b.quantity!=0){
-                    System.out.println("Book available. Ready fo Issue");
-                    return;
+            if(b.bookId.equals(biD)){
+                int issuednumbers = issued.containsKey(biD) ? issued.get(biD).size() : 0;
+
+                System.out.println("Book ID : " + biD);
+                System.out.println("Book Name : " + b.bookName);
+                System.out.println("Total Copies : " + b.quantity);
+                System.out.println("Currently Available : " + (b.quantity-issuednumbers));
+                if(issuednumbers>0){
+                    System.out.println("Issued Members : "  + issued.get(biD));
                 }
                 else{
-                    System.out.println("Sorry Book not availble. Come after some time");
-                    return;
+                    System.out.println("No Member currently holding this book");
                 }
+                return;
             }
+
         }
-        System.out.println("Invalid Book Id");
+        System.out.println("Invalid BookId");
     }
 }
 
-public class LibraryManagementSystem{
+//MANAGEMENT
+class LibraryManagementSystem{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Library lb = new Library();
 
         while(true){
-            System.out.println("\n=====LIBRARY MANAGEMENT SYSTEM=====");
-            System.out.println("1. Add Book");
-            System.out.println("2. Display Books");
-            System.out.println("3. Remove Books");
-            System.out.println("4. Search Books");
-            System.out.println("5. Add Member");
-            System.out.println("6. Display Members");
-            System.out.println("7. Remove Members");
-            System.out.println("8. Issue Book");
-            System.out.println("9. Return Book");
-            System.out.println("10. Check Book Availability");
-            System.out.println("11. Track books");
-            System.out.println("12. Exit");
+            System.out.println("\n +++++ LIBRARY MANAGEMENT SYSTEM +++++");
+            System.out.println("1. Add Books");
+            System.out.println("2. Update Book Quantity");
+            System.out.println("3. Search Books");
+            System.out.println("4. Remove Books");
+            System.out.println("5. Display Books");
+            System.out.println("6. Add Members");
+            System.out.println("7. Search Members");
+            System.out.println("8. Remove Members");
+            System.out.println("9. Display Members");
+            System.out.println("10. Issue Books");
+            System.out.println("11. Return Books");
+            System.out.println("12. Check Book Availability");
+            System.out.println("13. Track Books");
+            System.out.println("14. Exit");
 
-            System.out.print("\nEnter your choice : ");
+
+            System.out.print("\nEnter your choice(1-5) : ");
             int choice = sc.nextInt();
             sc.nextLine();
 
             switch(choice){
                 case 1:
-                    System.out.print("Enter Book Id : ");
-                    String id = sc.nextLine();
-                    System.out.print("Enter Book Title : ");
-                    String name = sc.nextLine();
-                    System.out.print("Enter Author name : ");
-                    String aname = sc.nextLine();
+                    System.out.print("\nEnter BookID : ");
+                    String bookId = sc.nextLine();
+                    System.out.print("Enter BookName : ");
+                    String bookName = sc.nextLine();
+                    System.out.print("Enter AuthorName : ");
+                    String authorName = sc.nextLine();
                     System.out.print("Enter Quantity : ");
-                    int qty = sc.nextInt();
-                    lb.addBooks(new Book(id, name, aname, qty));
+                    int quantity = sc.nextInt();
+                    lb.addBooks(new Book(bookId, bookName, authorName, quantity));
                     break;
                 case 2:
-                    lb.displayBooks();
+                    System.out.print("\nEnter the BookId to Update : ");
+                    String bookId1 = sc.nextLine();
+                    lb.updateQuantity(bookId1);
                     break;
                 case 3:
-                    System.out.print("Enter the BookId to remove : ");
-                    String bIId = sc.nextLine();
-                    lb.removeBooks(bIId);
+                    System.out.print("\nEnter the BookName to search : ");
+                    String bookName1 = sc.nextLine();
+                    lb.searchBooks(bookName1);
                     break;
                 case 4:
-                    System.out.print("Enter the Book title to search : ");
-                    String btitle = sc.nextLine();
-                    lb.searchBooks(btitle);
+                    System.out.print("\nEnter the BookId to remove : ");
+                    String bookId2 = sc.nextLine();
+                    lb.removeBooks(bookId2);
                     break;
                 case 5:
-                    System.out.print("Enter Member Id : ");
-                    String mId = sc.nextLine();
-                    System.out.print("Enter member name : ");
-                    String mname = sc.nextLine();
-                    lb.addMembers(new Member(mId, mname));
+                    lb.displayBooks();
                     break;
                 case 6:
-                    lb.displayMembers();
+                    System.out.print("\nEnter MemberId : ");
+                    String memId = sc.nextLine();
+                    System.out.print("Enter MemberName : ");
+                    String memName = sc.nextLine();
+                    lb.addMembers(new Member(memId, memName));
                     break;
                 case 7:
-                    System.out.println("Enter the MemberId to remove : ");
-                    String membId = sc.nextLine();
-                    lb.removeMembers(membId);
+                    System.out.print("\nEnter the MemberId to search : ");
+                    String memId1 = sc.nextLine();
+                    lb.searchMembers(memId1);
                     break;
                 case 8:
-                    System.out.print("Enter the Book Id to get : ");
-                    String bId = sc.nextLine();
-                    System.out.print("Please provide your member Id : ");
-                    String mIdd = sc.nextLine();
-                    lb.issueBooks(bId, mIdd);
+                    System.out.print("\nEnter the MemberId to remove : ");
+                    String memId2 = sc.nextLine();
+                    lb.removeMembers(memId2);
                     break;
                 case 9:
-                    System.out.print("Enter the Book Id to return : ");
-                    String bIdd = sc.nextLine();
-                    System.out.print("Enter your MemberId : ");
-                    String mIddd = sc.nextLine();
-                    lb.returnBooks(bIdd, mIddd);
+                    lb.displayMembers();
                     break;
                 case 10:
-                    System.out.print("Enter the Book Id to check availability : ");
-                    String bookID = sc.nextLine();
-                    lb.checkAvailability(bookID);
+                    System.out.print("\nEnter the BookId : ");
+                    String bIDD = sc.nextLine();
+                    System.out.print("Enter the MemberId : ");
+                    String mIDD = sc.nextLine();
+                    lb.issueBooks(bIDD, mIDD);
                     break;
                 case 11:
-                    System.out.print("Enter the Book Id to track : ");
-                    String tbId = sc.nextLine();
-                    lb.trackBooks(tbId);
+                    System.out.print("\nEnter the BookId to return : ");
+                    String bid = sc.nextLine();
+                    System.out.print("Enter the MemberId : ");
+                    String mid = sc.nextLine();
+                    lb.returnBooks(bid, mid);
                     break;
                 case 12:
-                    System.out.println("Exiting... Bye Bye");
-                    sc.close();
+                    System.out.print("\nEnter the BookId to check status : ");
+                    String boookId = sc.nextLine();
+                    lb.checkAvailability(boookId);
                     break;
+                case 13:
+                    System.out.print("\nEnter the BookId to Track : ");
+                    String bIId = sc.nextLine();
+                    lb.trackBooks(bIId);
+                    break;
+                case 14:
+                    System.out.println("\nExiting.....");
+                    System.exit(0);
                 default:
-                    System.out.println("Invalid Choice try again");
+                    System.out.println("Invalid choice. Try again...");
             }
-        }
+        }    
     }
-    
 }
