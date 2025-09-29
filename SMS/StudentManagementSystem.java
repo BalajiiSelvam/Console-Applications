5313e1ab33ff13adaa893ab95858860908c41f43
BalajiSelvam <balajiselvam245@gmail.com>
2025-09-29 09:18:35 +0530

    Added some meta data to SMS

--- SMS/StudentManagementSystem.java
+++ SMS/StudentManagementSystem.java
@@ -7,6 +7,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/* * A SIMPLE STUDENT MANAGEMENT SYSTEM WITH BELOW LISTED FEATURES
+   * 1. Adding students
+   * 2. Updating studdent details
+   * 3. Removing students
+   * 4. Displaying student details
+   * 5. Seraching a student by Roll NUmber
+   * 6. Adding marks
+   * 7. Calculating percentage and remarks
+   * 8. Generate Overall student report 
+   * 9. Download the students data as text files
+   * 10. Exiting the system
+   * 
+   * 
+   * Author : BalajiSelvam
+   * Java Developer
+   */
+
 class Student{
     String rollNo;
     String name;
